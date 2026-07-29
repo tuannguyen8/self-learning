67df89aa508e748694b3798050180dbbec65b2b3
Tuan Nguyen <nguyenvutuan1@gmail.com>
2026-07-28 21:35:34 -0700

    11, medium level, 2 pointers

---
+++ 11-2pointers-medium-Container-With-Most-Water.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size()-1;
+        int result = 0;
+        while (i<j){
+            int area = min(height[i], height[j]) * (j-i);
+            result = max(area, result);
+            if(height[i]>height[j]){
+                j--;
+            }else{
+                i++;
+            }
+        }
+        return result;
+    }
+};
