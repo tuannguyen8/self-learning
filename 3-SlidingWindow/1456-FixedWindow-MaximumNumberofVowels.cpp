94e59697233e37903f1e0b95cf31498410e205bd
Tuan Nguyen <nguyenvutuan1@gmail.com>
2026-07-29 23:50:21 -0700

    1456, fixed window, max num of vowels in a sub of given length

---
+++ 3-SlidingWindow/1456-FixedWindow-MaximumNumberofVowels.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isVowel(char c){
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    int maxVowels(string s, int k) {
+        int vowelCount = 0;
+        for(int i = 0; i<k; i++){
+            if(isVowel(s[i])){
+                vowelCount++;
+            }
+        }
+        int maxCount = vowelCount;
+        cout<< "1" << maxCount<< endl;
+        for(int i = k; i<s.size(); i++){
+            if(isVowel(s[i])){
+                vowelCount++;
+            }
+            if(isVowel(s[i-k])){
+                vowelCount--;
+            }
+            maxCount = max(maxCount, vowelCount);
+        }
+        return maxCount;
+    }
+};
